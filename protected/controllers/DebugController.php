4c423e827ddae0ee3fe8b7810289cf7cbaa0b16e
Bear90 <soza.mihail@gmail.com>
2016-02-29 19:04:10 +0300

    Fixes

--- protected/controllers/DebugController.php
+++ protected/controllers/DebugController.php
@@ -5,26 +5,6 @@ namespace application\controllers;
 class DebugController extends \CController {
 
     public function actionIndex(){
-        ini_set('safe_mode', true);
-        \Yii::import('application.extensions.yii-mail.YiiMailMessage');
 
-        $subject = 'test subject';
-        $from = 'admin@motivator.com';
-        $to = 'soza.mihail@gmail.com';
-
-        $message = new \YiiMailMessage;
-        $message->view = 'test';
-
-        $message->setSubject($subject)
-            ->setFrom($from)
-            ->setTo($to);
-         
-        //userModel is passed to the view
-        //$message->setBody($data, 'text/html');
-         
-        $message->from = \Yii::app()->params['adminEmail'];
-        $r = null;
-
-        var_dump(\Yii::app()->mail->send($message, $r));
     }
 }
