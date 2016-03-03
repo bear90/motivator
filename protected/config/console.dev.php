e7a792aca362ef40ffceb1f268b929e4f283eba3
bear <soza.mihail@gmail.com>
2016-03-04 01:51:56 +0300

    update

---
+++ protected/config/console.dev.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author soza.mihail@gmail.com
+ */
+return array(
+    'components' => array(
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=motivator',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => 'start123',
+            'charset' => 'utf8',
+        )
+    ),
+    'params' => array(
+        'yiiPath'       => 'd:\www/motivator.by/framework/yii.php',
+        'yiicPath'       => 'd:\www/motivator.by/framework/yiic.php'
+    )
+);
---
+++ protected/migrations/m160303_215445_update_tourists.php
@@ -0,0 +1,25 @@
+<?php
+
+class m160303_215445_update_tourists extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m160303_215445_update_tourists does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
