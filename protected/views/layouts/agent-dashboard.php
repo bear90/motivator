3de3c2d2f0ea44f56cac31b1657ecb353e2a3599
bear <soza.mihail@gmail.com>
2016-11-17 23:48:25 +0300

    Fix path for favicon on some pages

--- protected/views/layouts/agent-dashboard.php
+++ protected/views/layouts/agent-dashboard.php
@@ -7,7 +7,7 @@
     <meta content="Motivator" name='description'>
     <meta content="Motivator" name='keywords'>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="shortcut icon" href="/img/favicon.ico">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/plugins/slidebars.css">
     <link rel="stylesheet" href="/css/modules/tab-touragents.css">
