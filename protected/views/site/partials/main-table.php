075611365fd3ab30d9470eabf8eeaf7b2bb07b5e
bear <soza.mihail@gmail.com>
2017-06-12 08:39:16 +0300

    fix Main table

--- protected/views/site/partials/main-table.php
+++ protected/views/site/partials/main-table.php
@@ -17,38 +17,38 @@
         <th>Самое <br>выгодное<br> предложение</th>
     </tr>
     <tr>
-        <td>21.05.2017
-            Алексей
+        <td>21.05.2017 <br>
+            Алексей<br>
             № 121</td>
-        <td>Египет/
+        <td>Египет/<br>
             отдых на море</td>
-        <td>Взрослых: 2 чел.
+        <td>Взрослых: 2 чел.<br>
             Детей: 1 (5 лет),
             1 (7 лет).</td>
-        <td>12 дней/ 20.07.2017</td>
+        <td>12 дней/<br> 20.07.2017</td>
         <td>890€</td>
     </tr>
     <tr>
-        <td>18.05.2017
-            Наталья
+        <td>18.05.2017<br>
+            Наталья<br>
             № 120</td>
-        <td>Литва-Польша/
+        <td>Литва-Польша/<br>
             шоп-тур</td>
         <td>Взрослых: 3 чел.</td>
-        <td>5 дней/ 25.05.2017</td>
+        <td>5 дней/<br> 25.05.2017</td>
         <td>
             <div class="block"><span class="glyphicon glyphicon-fire"></span>515€</div>
             <div class="block"><span class="glyphicon glyphicon-asterisk"></span>420€</div>
         </td>
     </tr>
     <tr>
-        <td>17.05.2017
-            Михаил
+        <td>17.05.2017<br>
+            Михаил<br>
             № 119</td>
-        <td>Турция/
+        <td>Турция/<br>
             свадебный тур</td>
         <td>Взрослых: 2 чел.</td>
-        <td>14 дней/ 01.08.2017</td>
+        <td>14 дней/<br> 01.08.2017</td>
         <td>
             <div class="block"><span class="glyphicon glyphicon-asterisk"></span>490€</div>
         </td>
