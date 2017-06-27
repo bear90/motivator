302773c1247252260d5f130a21ebe8e462189f33
bear <soza.mihail@gmail.com>
2017-06-27 08:01:47 +0300

    Change ordering for offers

--- protected/views/site/partials/offer-list.php
+++ protected/views/site/partials/offer-list.php
@@ -2,40 +2,40 @@
     use application\models\defines\Offer\PriceType;
 ?>
 
-<?php foreach($offers as $num => $offer) : ?>
+<?php for($i = count($offers); $i>0; $i--) : ?>
 
-    <div class="row" id="offer_<?php echo $offer->id; ?>">
+    <div class="row" id="offer_<?php echo $offers[$i-1]->id; ?>">
         <div class="col-md-12">
-            <h3>Предложение №<?php echo $num+1; ?></h3>
+            <h3>Предложение №<?php echo $i; ?></h3>
 
-            <?php if($showContactForFirtsOne && $num==0): ?>
-            <div><?php echo $offer->contact; ?></div>
+            <?php if($showContactForFirtsOne && $i==count($offers)): ?>
+            <div><?php echo $offers[$i-1]->contact; ?></div>
             <?php endif; ?>
 
-            <div><?php echo $offer->description; ?></div>
+            <div><?php echo $offers[$i-1]->description; ?></div>
 
-            <?php if(!is_null($offer->price)) : ?>
+            <?php if(!is_null($offers[$i-1]->price)) : ?>
                 <div>
-                    Штатная продажа: <?php echo ceil($offer->price); ?>
+                    Штатная продажа: <?php echo ceil($offers[$i-1]->price); ?>
                 </div>
             <?php endif; ?>
 
-            <?php if(!is_null($offer->earlyPrice)) : ?>
+            <?php if(!is_null($offers[$i-1]->earlyPrice)) : ?>
                 <div>
                     Ранее бронирование: 
-                    <i class="fa fa-snowflake-o" aria-hidden="true"></i> <?php echo ceil($offer->earlyPrice); ?>
+                    <i class="fa fa-snowflake-o" aria-hidden="true"></i> <?php echo ceil($offers[$i-1]->earlyPrice); ?>
                 </div>
             <?php endif; ?>
 
-            <?php if(!is_null($offer->lastMinPrice)) : ?>
+            <?php if(!is_null($offers[$i-1]->lastMinPrice)) : ?>
                 <div>
                     Горящий тур:
-                    <span class="glyphicon glyphicon-fire"></span> <?php echo ceil($offer->lastMinPrice); ?>
+                    <span class="glyphicon glyphicon-fire"></span> <?php echo ceil($offers[$i-1]->lastMinPrice); ?>
                 </div>
             <?php endif; ?>
         </div>
     </div>
 
-<?php endforeach;?>
+<?php endfor;?>
 
 <button type="button" class="btn btn-default btn-green add-offer2" data-id="<?php echo $taskId; ?>">Разместить предложение</button>
