ae07ff9d9b3bbbd3f2ccab1b6f69d1dd1b5bae8b
bear <soza.mihail@gmail.com>
2017-06-30 07:55:38 +0300

    test

--- protected/controllers/site/TestAction.php
+++ protected/controllers/site/TestAction.php
@@ -11,8 +11,11 @@ class TestAction extends \CAction
     {
         $tourist = 'soza.mihail@gmail.com';
 
-        if ($tourist)
-        {
+        if (\Yii::app()->request->getParam('mail')) {
+            $message = "Line 1\r\nLine 2\r\nLine 3";
+            $message = wordwrap($message, 70, "\r\n");
+            var_dump(mail($tourist, 'My Subject', $message));
+        } else {
             $sent = \Tool::sendEmailWithView($tourist, 'add-task');
 
             var_dump($sent);
