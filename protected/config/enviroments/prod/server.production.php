8713bc6096f75be628d62d95b88ddadc11c2c725
bear <soza.mihail@gmail.com>
2017-07-20 21:12:43 +0300

    fix configs

--- protected/config/enviroments/prod/server.production.php
+++ protected/config/enviroments/prod/server.production.php
@@ -17,7 +17,7 @@ return array(
     'params'=>array(
         'helpEmail'    =>'help@motivator-travel.by',
         'adminEmail'    =>'abonent@penki.by',
-        'senderEmail' => 'webmaster@d0012280.atservers.net',
+        'senderEmail' => 'webmaster@penki.by',
         'yiiPath'       => '/home/user2007762/frameworks/1.1.17/framework/yii.php',
     ),
 );
