007034c7c6706d00ef586c0c494682462dc55f2c
bear <soza.mihail@gmail.com>
2017-08-07 07:32:00 +0300

    fix logo

--- protected/views/layouts/turagentam.php
+++ protected/views/layouts/turagentam.php
@@ -45,7 +45,7 @@
             <div class="row">
                 <div class="col-xs-12 col-xs-offset-0 col-sm-4 col-sm-offset-4 col-lg-3 col-lg-offset-0">
                     <a class="logo" href="/">
-                        <h2 class="text-right text-logo"><span>П<span>ортал</span> <i>penki.by</i></span></h2>
+                        <h2 class="text-right text-logo"><span>П<span>ортал</span><i>penki.by</i></span></h2>
                     </a>
                 </div>
                 <div class="col-md-6" id="footer-menu">
