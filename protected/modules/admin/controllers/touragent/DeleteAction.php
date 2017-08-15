674a9bebe4f360cedb47567c226947d4722bd992
bear90 <soza.mihail@gmail.com>
2017-08-15 18:00:39 +0300

    Remove User of the touragent on touragent deletion

--- protected/modules/admin/controllers/touragent/DeleteAction.php
+++ protected/modules/admin/controllers/touragent/DeleteAction.php
@@ -6,6 +6,7 @@
 namespace application\modules\admin\controllers\touragent;
 
 use application\models\entities\Touragent;
+use application\models\entities\User;
 
 class DeleteAction extends \CAction
 {
@@ -13,7 +14,11 @@ class DeleteAction extends \CAction
     public function run($id)
     {
 
-        Touragent::model()->deleteByPk($id);
+        $touragent = Touragent::model()->findByPk($id);
+        if ($touragent) {
+            $touragent->delete();
+            User::model()->deleteByPk($touragent->userId);
+        }
         \Yii::app()->user->setFlash('message', "Турагент удален.");
         $this->controller->redirect(\Yii::app()->createUrl("/admin/touragent"));
     }
