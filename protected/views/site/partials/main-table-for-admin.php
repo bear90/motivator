ca4ac68eaf0950d56a0ae4f4b563820706558770
bear <soza.mihail@gmail.com>
2017-09-17 09:55:07 +0300

    add noindex for emails

--- protected/views/site/partials/main-table-for-admin.php
+++ protected/views/site/partials/main-table-for-admin.php
@@ -33,7 +33,7 @@
                 № <?php echo $model->data()->id; ?><br>
                 <?php echo $model->createdAt(); ?><br>
                 <?php echo $model->data()->relName->name; ?><br>
-                <?php echo $model->data()->email; ?><br>
+                <noindex><?php echo $model->data()->email; ?></noindex><br>
                 <a href="#" class="offers-link">подробнее</a><br>
             </td>
 
